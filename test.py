4c8aef5510d95378e16cfb92d7357223d651cdcd
Fighting666888 <57851691+Fighting666888@users.noreply.github.com>
2019-11-17 11:57:49 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 17 09:45:56 2019
+
+@author: Administrator
+"""
+from skimage import io
+img = io.imread('C:/Users/Administrator/Desktop/520941061845014415.jpg')
+io.imshow(img)
